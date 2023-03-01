e9976a0bfe4b28974889eeaa72527467876484b5
MrGab0uwu <gabriel.escobar.torres@gmail.com>
2023-02-28 23:18:31 -0600

    fetch gps data from supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-/* global google */
+// global google
 import {
 	GoogleMap,
 	InfoWindow,
 	MarkerF,
 	useLoadScript,
 } from '@react-google-maps/api';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { supabase } from './supabase/client';
 
 const App = () => {
 	const { isLoaded } = useLoadScript({
@@ -15,22 +16,50 @@ const App = () => {
 	const [mapRef, setMapRef] = useState();
 	const [isOpen, setIsOpen] = useState(false);
 	const [infoWindowData, setInfoWindowData] = useState();
-	const markers = [
-		{ address: 'Address1', lat: 17.99555, lng: -94.55532 },
-		{ address: 'Address2', lat: 17.99155, lng: -94.54132 },
-		{ address: 'Address3', lat: 17.99445, lng: -94.55112 },
-	];
+
+	const [markers, setMarkers] = useState([]);
+
+	const [counter, setCounter] = useState(4);
+	useEffect(() => {
+		const fetchApi = async () => {
+			const { data } = await supabase
+				.from('gps-data')
+				.select()
+				.eq('id', counter);
+			setCounter(counter + 1);
+			console.log(counter);
+			console.log(data);
+			const newData = data.map(({ lat, lng, type }) => {
+				const latInt = parseFloat(lat);
+				const lngInt = parseFloat(lng);
+
+				return {
+					lat: latInt,
+					lng: lngInt,
+					type,
+				};
+			});
+			console.log(newData);
+			setMarkers(newData);
+		};
+
+		const interval = setInterval(() => {
+			fetchApi();
+		}, 3000);
+
+		return () => clearInterval(interval);
+	}, [markers, counter]);
 
 	const onMapLoad = (map) => {
 		setMapRef(map);
-		const bounds = new google.maps.LatLngBounds();
-		markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
-		map.fitBounds(bounds);
+		// const bounds = new google.maps.LatLngBounds();
+		// markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+		// map.fitBounds(bounds);
 	};
 
-	const handleMarkerClick = (id, lat, lng, address) => {
+	const handleMarkerClick = (id, lat, lng, type) => {
 		mapRef?.panTo({ lat, lng });
-		setInfoWindowData({ id, address });
+		setInfoWindowData({ id, type });
 		setIsOpen(true);
 	};
 
@@ -41,15 +70,17 @@ const App = () => {
 			) : (
 				<GoogleMap
 					mapContainerClassName='map-container'
+					center={{ lat: 17.996528, lng: -94.557722 }}
 					onLoad={onMapLoad}
 					onClick={() => setIsOpen(false)}
+					zoom={1}
 				>
-					{markers.map(({ address, lat, lng }, ind) => (
+					{markers.map(({ type, lat, lng }, ind) => (
 						<MarkerF
 							key={ind}
 							position={{ lat, lng }}
 							onClick={() => {
-								handleMarkerClick(ind, lat, lng, address);
+								handleMarkerClick(ind, lat, lng, type);
 							}}
 						>
 							{isOpen && infoWindowData?.id === ind && (
@@ -58,7 +89,7 @@ const App = () => {
 										setIsOpen(false);
 									}}
 								>
-									<h3 className='p-5'>{infoWindowData.address}</h3>
+									<h3 className='p-5'>{infoWindowData.type}</h3>
 								</InfoWindow>
 							)}
 						</MarkerF>
